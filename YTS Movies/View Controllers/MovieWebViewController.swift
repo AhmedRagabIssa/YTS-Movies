8792177d5c96c68f4c08b958859195afb2a8ca0e
Ahmed Ragab Issa <ar2007eg@gmail.com>
2018-07-02 15:45:45 +0200

    adding activity indicator to the web view

--- YTS Movies/View Controllers/MovieWebViewController.swift
+++ YTS Movies/View Controllers/MovieWebViewController.swift
@@ -9,20 +9,44 @@
 import UIKit
 import WebKit
 
-class MovieWebViewController: UIViewController {
+class MovieWebViewController: UIViewController, UIWebViewDelegate{
 
     @IBOutlet weak var webView: WKWebView!
     
     var movieURL: String!
     
+    // this activity indecator to be showen while featching data from the internet
+    var activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        initConfig()
+        
         let url = URL(string: self.movieURL ?? "https://yts.am/")
         let requst = URLRequest(url: url!)
         
         webView.load(requst)
     }
+    
+    func initConfig(){
+        // activity indecator init configuration
+        activityIndicator.center = self.view.center
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.activityIndicatorViewStyle = .gray
+        
+        self.startActivityIndicator()
+    }
+    
+    func webViewDidFinishLoad(_ webView: UIWebView){
+        activityIndicator.stopAnimating()
+    }
 
+    // this func is for start the activity indicator and disable the user interaction events
+    func startActivityIndicator(){
+        self.view.addSubview(activityIndicator)
+        activityIndicator.startAnimating()
+    }
 
 }
